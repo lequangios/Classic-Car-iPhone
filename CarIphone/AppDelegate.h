324e6061d60cad6476a50e6b35cc0c39312ddc3b
Lê Việt Quang <levietquang.ios.dev@gmail.com>
2014-02-24 07:07:40 +0700

    Initial Commit

---
+++ CarIphone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarIphone
+//
+//  Created by Le Viet Quang on 2/24/14.
+//  Copyright (c) 2014 Le Viet Quang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarIphoneTests/CarIphoneTests.m
@@ -0,0 +1,34 @@
+//
+//  CarIphoneTests.m
+//  CarIphoneTests
+//
+//  Created by Le Viet Quang on 2/24/14.
+//  Copyright (c) 2014 Le Viet Quang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CarIphoneTests : XCTestCase
+
+@end
+
+@implementation CarIphoneTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
